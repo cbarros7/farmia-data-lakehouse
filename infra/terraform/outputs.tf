11034ef0fd53c9c37e1d44552ba025c57069429e
Carlos Barros <cj.barros07@gmail.com>
2026-05-16 13:30:26 +0200

    feat :wrench: Se agrega configuracion de Terraform

---
+++ infra/terraform/outputs.tf
@@ -0,0 +1,31 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.core.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.adls.name
+}
+
+output "adls_dfs_endpoint" {
+  value = "${azurerm_storage_account.adls.name}.dfs.core.windows.net"
+}
+
+output "adls_containers" {
+  value = local.adls_containers
+}
+
+output "eventhub_namespace" {
+  value = azurerm_eventhub_namespace.telemetry.name
+}
+
+output "eventhub_name" {
+  value = azurerm_eventhub.telemetry.name
+}
+
+output "databricks_workspace_url" {
+  value = azurerm_databricks_workspace.compute.workspace_url
+}
+
+output "databricks_workspace_id" {
+  value = azurerm_databricks_workspace.compute.id
+}
