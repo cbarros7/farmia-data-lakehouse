d9e6b008954bad52b98187da2945864ade4aac0a
Carlos Barros <cj.barros07@gmail.com>
2026-05-18 15:36:02 +0200

    feat :hammer: Actualizacion de DDL para que sean coherentes con contratos YAML

--- infra/databricks/queries/tables/silver_iot_sensors.sql
+++ infra/databricks/queries/tables/silver_iot_sensors.sql
@@ -1,24 +1,25 @@
 CREATE TABLE IF NOT EXISTS silver.iot_sensors (
     `sensor_id` STRING NOT NULL,
-    `location` STRING,
+    `location_id` STRING NOT NULL,
     `temperature` DOUBLE,
     `humidity` DOUBLE,
     `ph` DOUBLE,
-    `reading_timestamp` TIMESTAMP,
-    `reading_date` DATE,
-    `batch_id` STRING NOT NULL,
-    `ingest_timestamp` TIMESTAMP NOT NULL,
-    `event_timestamp` TIMESTAMP NOT NULL,
-    `schema_version_id` STRING NOT NULL,
-    `pipeline_git_hash` STRING NOT NULL,
-    `source_system` STRING NOT NULL,
-    `file_path` STRING NOT NULL,
-    `execution_user_id` STRING NOT NULL,
-    `environment_id` STRING NOT NULL,
-    `cluster_id` STRING NOT NULL,
-    `processing_library_version` STRING NOT NULL,
-    `operation_type` STRING NOT NULL,
-    `retention_ttl` INT NOT NULL
+    `soil_moisture` DOUBLE,
+    `reading_timestamp` TIMESTAMP NOT NULL,
+    `reading_date` DATE NOT NULL,
+    `_batch_id` BIGINT NOT NULL,
+    `_ingest_timestamp` TIMESTAMP NOT NULL,
+    `_event_timestamp` TIMESTAMP NOT NULL,
+    `_schema_version_id` STRING NOT NULL,
+    `_pipeline_git_hash` STRING NOT NULL,
+    `_source_system` STRING NOT NULL,
+    `_file_path` STRING NOT NULL,
+    `_execution_user_id` STRING NOT NULL,
+    `_environment_id` STRING NOT NULL,
+    `_cluster_id` STRING NOT NULL,
+    `_processing_library_version` STRING NOT NULL,
+    `_operation_type` STRING NOT NULL,
+    `_retention_ttl` STRING NOT NULL
 )
 USING DELTA
 PARTITIONED BY (sensor_id, reading_date)
@@ -30,4 +31,5 @@ TBLPROPERTIES (
     'idempotence_mode' = 'APPEND'
 );
 
-COMMENT ON TABLE silver.iot_sensors IS 'Granular IoT telemetry (temp, humidity, pH). TTL: 14 días. Pre-agg obligatorio antes de VACUUM.';
+COMMENT ON TABLE silver.iot_sensors IS 'Granular IoT telemetry (temp, humidity, pH, soil_moisture). TTL: 14 días. Pre-agg obligatorio antes de VACUUM.';
+
